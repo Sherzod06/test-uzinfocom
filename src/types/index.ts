0176ffbe250c94b270f4ed3fd48b2d12e2336d1c
Sherzod Mangliev <sherzod_mangliev@outlook.com>
2024-03-23 19:58:38 +0500

    common layout

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    model: string,
+    released_date: number,
+    category: string,
+    price: string
+    description?: string,
+    created_at: string,
+    is_visible: boolean,
+    image?: string
+}
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div style="width: 100%">
+        <div>
+            <el-table :data="tableData" height="250" style="width: 100%">
+                <el-table-column prop="date" label="Date" width="180" />
+                <el-table-column prop="name" label="Name" width="180" />
+                <el-table-column prop="address" label="Address" />
+                <el-table-column prop="address" label="Address" />
+                <el-table-column prop="address" label="Address" />
+                <el-table-column prop="address" label="Address" />
+                <el-table-column prop="address" label="Address" />
+                <el-table-column prop="address" label="Address" />
+                <el-table-column prop="address" label="Address" />
+            </el-table>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
